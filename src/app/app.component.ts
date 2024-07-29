8fdafb7bba6b81d4275fd651914f44a6e2f4e5f3
Rahul Bali <rahulrdb18@gmail.com>
2024-07-29 17:15:59 +0530

    oikn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['../demo-styling.css']
+  styleUrls: ['../demo-styling.css'],
+  imports: [NgOptimizedImage],
 })
 export class AppComponent {
-  title = 'angular-quickstart';
+  title = 'rahul-bali-astrology';
 }
