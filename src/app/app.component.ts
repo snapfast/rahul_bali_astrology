277468a5b5be120d8201ad3445a12f42b12c537b
Rahul Bali <rahulrdb18@gmail.com>
2024-07-29 17:19:26 +0530

    update fix for NgOptimizedImage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['../demo-styling.css'],
-  imports: [NgOptimizedImage],
 })
 export class AppComponent {
   title = 'rahul-bali-astrology';
