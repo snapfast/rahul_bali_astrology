92d9a742bce4198bbd1c6e47e8673729af9e4110
Rahul Bali <rahulrdb18@gmail.com>
2024-07-31 16:22:11 +0530

    consultation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConsultationComponent } from './consultation';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'consultation', component: ConsultationComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
