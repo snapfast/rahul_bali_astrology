a67cc6688cc052f46c93be6a5e1bb3826f14fa5e
Rahul Bali <rahulrdb18@gmail.com>
2024-07-31 16:32:04 +0530

    rfew

--- src/app/consultation/consultation.component.ts
+++ src/app/consultation/consultation.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'consultation',
   templateUrl: './consultation.component.html',
-  styleUrls: ['../consultation/demo-styling.css'],
+  styleUrls: ['./consultation.component.css'],
 })
 export class ConsultationComponent {
   title = 'rahul-bali-astrology';
