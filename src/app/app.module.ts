11af1ff06ca4d591d8a2f22bc363e7a04bd08901
Rahul Bali <rahulrdb18@gmail.com>
2024-08-02 06:40:37 +0530

    added componenets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ConsultationComponent } from './consultation/consultation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ConsultationComponent,
   ],
   imports: [
     BrowserModule,
