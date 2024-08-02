249bbaa9f310e570eb05e1e32bb31c214fffb59d
Rahul Bali <rahulrdb18@gmail.com>
2024-08-02 14:20:42 +0530

    hfdklsa

---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'consultation',
+  templateUrl: './consultation.component.html',
+  styleUrls: ['./consultation.component.css'],
+})
+export class ConsultationComponent {
+  title = 'rahul-bali-astrology';
+}
