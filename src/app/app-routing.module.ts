5da0e3aba72e70eb98e8d542bdc9347b9e6afc79
Rahul Bali <rahulrdb18@gmail.com>
2024-08-02 05:56:36 +0530

    bhh

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { ConsultationComponent } from './consultation/consultation.component';
 
 const routes: Routes = [
