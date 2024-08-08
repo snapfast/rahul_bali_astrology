774f4a62d3c2f33fffc9f35a0f57eb3572117ce3
Rahul Bali <rahulrdb18@gmail.com>
2024-08-08 15:08:31 +0530

    send

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ConsultationComponent } from './consultation/consultation.component';
 import { ContactComponent } from './contact/contact.component';
+import { PaymentComponent } from './payment/payment.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'consultation', component: ConsultationComponent },
+  { path: 'payment', component: PaymentComponent },
   { path: 'contact', component: ContactComponent}
 ];
 
---
+++ src/app/payment/payment.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subject, timer } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+@Component({
+  selector: 'payment',
+  templateUrl: './payment.component.html',
+  styleUrls: ['./payment.component.css'],
+  imports: [MatProgressSpinnerModule],
+})
+export class PaymentComponent implements OnInit, OnDestroy {
+  public remainingTime: string = '00:00';
+  private endTime: Date = new Date(); // Initialize to current date
+  private destroy$: Subject<boolean> = new Subject<boolean>();
+  qr = false;
+
+  ngOnInit() {
+    this.initializeTimer();
+  }
+
+  initializeTimer() {
+    
+    // After 3 seconds, replace spinner with image
+    setTimeout(() => {
+      this.qr = true;
+    }, 3000);
+
+    // Set the countdown duration (e.g., 5 minutes)
+    const duration = 5 * 60 * 1000; // 5 minutes in milliseconds
+    this.endTime = new Date(new Date().getTime() + duration);
+
+    timer(0, 1000).pipe(takeUntil(this.destroy$)).subscribe(() => {
+      const now = new Date();
+      const distance = this.endTime.getTime() - now.getTime();
+
+      if (distance <= 0) {
+        this.remainingTime = '00:00';
+        this.handleTimeUp();
+        this.destroy$.next(true);
+        this.destroy$.complete();
+      } else {
+        const minutes = Math.floor(distance / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        this.remainingTime = `${this.pad(minutes)}:${this.pad(seconds)}`;
+      }
+    });
+  }
+
+  pad(value: number): string {
+    return value < 10 ? `0${value}` : value.toString();
+  }
+
+  handleTimeUp() {
+    alert('Time is up! Redirectly to previous page.');
+    // You can add any additional logic here, such as redirecting or displaying a message
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.complete();
+  }
+
+  delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+}
+
+
