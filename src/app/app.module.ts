412b71f0e255938ca9d31d0080185aa8193299ab
Rahul Bali <rahulrdb18@gmail.com>
2024-08-08 16:26:53 +0530

    things

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ConsultationComponent } from './consultation/consultation.component';
+import { PaymentComponent } from './payment/payment.component';
+import { MatProgressSpinnerModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ConsultationComponent,
+    PaymentComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatProgressSpinnerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
