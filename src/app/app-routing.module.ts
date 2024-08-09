024aba2deba759475eec41a0ada72f92f9a422ae
Rahul Bali <rahulrdb18@gmail.com>
2024-08-09 11:09:13 +0530

    linking about

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { ConsultationComponent } from './consultation/consultation.component';
 import { ContactComponent } from './contact/contact.component';
 import { PaymentComponent } from './payment/payment.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'consultation', component: ConsultationComponent },
   { path: 'payment', component: PaymentComponent },
   { path: 'contact', component: ContactComponent},
-  { path: 'contact', component: ContactComponent},
+  { path: 'about', component: AboutComponent},
 ];
 
 @NgModule({
